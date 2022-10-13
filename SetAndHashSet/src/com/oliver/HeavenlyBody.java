6b99c611cb90e56ac52d3d73f8c6ffa30fab89c4
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-10-13 16:41:46 -0500

    video 166

--- SetAndHashSet/src/com/oliver/HeavenlyBody.java
+++ SetAndHashSet/src/com/oliver/HeavenlyBody.java
@@ -43,4 +43,10 @@ public final class HeavenlyBody {
         String objName = ((HeavenlyBody) obj).getName();
         return this.name.equals(objName);
     }
+
+    @Override
+    public int hashCode() {
+        System.out.println("hashcode called");
+        return this.name.hashCode() + 57;
+    }
 }

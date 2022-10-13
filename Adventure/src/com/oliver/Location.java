21120f96524034e3b6ec08165643d472c1826ab7
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-10-13 14:59:58 -0500

    video 163

--- Adventure/src/com/oliver/Location.java
+++ Adventure/src/com/oliver/Location.java
@@ -6,17 +6,22 @@ import java.util.Map;
 public class Location {
     private final int locationID;
     private final String description;
-    private final Map<String, Integer> exits = new HashMap<>();
+    private final Map<String, Integer> exits; // = new HashMap<>();
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
+        if (exits != null) {
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
     public int getLocationID() {
         return locationID;
--- Adventure/src/com/oliver/Main.java
+++ Adventure/src/com/oliver/Main.java
@@ -8,27 +8,32 @@ public class Main {
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        locations.put(0, new Location(0, "front of PC"));
-        locations.put(1, new Location(1, "end of road in front of bridge"));
-        locations.put(2, new Location(2, "top of a hill"));
-        locations.put(3, new Location(3, "well house in small spring"));
-        locations.put(4, new Location(4, "valley beside stream"));
-        locations.put(5, new Location(5, "forest"));
+        Map<String, Integer> tempExit = new HashMap<>();
+        locations.put(0, new Location(0, "front of PC", tempExit));
 
-        locations.get(1).addExit("W",2);
-        locations.get(1).addExit("E",3);
-        locations.get(1).addExit("S",4);
-        locations.get(1).addExit("N",5);
+        tempExit.put("W",2);
+        tempExit.put("E",3);
+        tempExit.put("S",4);
+        tempExit.put("N",5);
+        locations.put(1, new Location(1, "end of road in front of bridge", tempExit));
 
-        locations.get(2).addExit("N",5);
+        tempExit = new HashMap<>();
+        tempExit.put("N",5);
+        locations.put(2, new Location(2, "top of a hill", tempExit));
 
-        locations.get(3).addExit("W",1);
+        tempExit = new HashMap<>();
+        tempExit.put("W",1);
+        locations.put(3, new Location(3, "well house in small spring", tempExit));
 
-        locations.get(4).addExit("N",1);
-        locations.get(4).addExit("W",2);
+        tempExit = new HashMap<>();
+        tempExit.put("N",1);
+        tempExit.put("W",2);
+        locations.put(4, new Location(4, "valley beside stream", tempExit));
 
-        locations.get(5).addExit("S",1);
-        locations.get(5).addExit("W",2);
+        tempExit = new HashMap<>();
+        tempExit.put("S",1);
+        tempExit.put("W",2);
+        locations.put(5, new Location(5, "forest", tempExit));
 
         Map<String, String> vocabulary = new HashMap<>();
         vocabulary.put("QUIT", "Q");
@@ -40,6 +45,7 @@ public class Main {
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
+//            tempExit.remove("S");
             if (loc == 0) {
                 break;
             }

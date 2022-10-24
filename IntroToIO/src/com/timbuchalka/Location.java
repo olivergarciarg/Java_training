e35f0b8399b9d2503d069671038a5dcc1f848724
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-10-24 13:17:17 -0500

    video 237b

--- IntroToIO/src/com/timbuchalka/Location.java
+++ IntroToIO/src/com/timbuchalka/Location.java
@@ -1,13 +1,9 @@
 package com.timbuchalka;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * Created by dev on 8/12/2015.
- */
 public class Location {
     private final int locationID;
     private final String description;
@@ -17,9 +13,9 @@ public class Location {
         this.locationID = locationID;
         this.description = description;
         if(exits != null) {
-            this.exits = new HashMap<String, Integer>(exits);
+            this.exits = new LinkedHashMap<String, Integer>(exits);
         } else {
-            this.exits = new HashMap<String, Integer>();
+            this.exits = new LinkedHashMap<String, Integer>();
         }
         this.exits.put("Q", 0);
     }
@@ -37,7 +33,7 @@ public class Location {
     }
 
     public Map<String, Integer> getExits() {
-        return new HashMap<String, Integer>(exits);
+        return new LinkedHashMap<String, Integer>(exits);
     }
 
     protected void addExit(String direction, int location) {

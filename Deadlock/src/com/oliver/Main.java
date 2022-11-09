82426911e5c8cdc231109dbb4670766587af9650
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2022-11-09 15:50:33 -0600

    video 282

--- Deadlock/src/com/oliver/Main.java
+++ Deadlock/src/com/oliver/Main.java
@@ -31,20 +31,20 @@ public class Main {
 
     private static class Thread2 extends Thread {
         public void run() {
-            synchronized (lock2) {
-                System.out.println("Thread2 has lock 2");
+            synchronized (lock1) {
+                System.out.println("Thread2 has lock 1");
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
 
                 }
-                System.out.println("Thread2 waiting for lock1");
-                synchronized (lock1) {
-                    System.out.println("Thread2 has lock2 and lock1");
+                System.out.println("Thread2 waiting for lock2");
+                synchronized (lock2) {
+                    System.out.println("Thread2 has lock1 and lock2");
                 }
-                System.out.println("Thread2 released lock1");
+                System.out.println("Thread2 released lock2");
             }
-            System.out.println("Thread 2 released lock2. Exiting...");
+            System.out.println("Thread 2 released lock1. Exiting...");
         }
     }
 }

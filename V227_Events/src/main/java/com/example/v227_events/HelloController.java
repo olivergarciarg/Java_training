5b697d0b1fe8243590e6e385249c3279fdfa3026
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2023-03-13 12:59:44 -0500

    v227

---
+++ V227_Events/src/main/java/com/example/v227_events/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.v227_events;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}

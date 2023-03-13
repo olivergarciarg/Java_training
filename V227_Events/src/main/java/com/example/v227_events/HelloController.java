05228e7141997313db42c18e6d36342924aab16d
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2023-03-13 15:57:51 -0500

    v229

--- V227_Events/src/main/java/com/example/v227_events/HelloController.java
+++ V227_Events/src/main/java/com/example/v227_events/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.v227_events;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField nameField;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button helloButton;
+
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    protected void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if(e.getSource().equals(helloButton)){
+            System.out.println("Hello, " + nameField.getText());
+        } else if(e.getSource().equals(byeButton)){
+            System.out.println("Bye, " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyRelease() {
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() | text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }

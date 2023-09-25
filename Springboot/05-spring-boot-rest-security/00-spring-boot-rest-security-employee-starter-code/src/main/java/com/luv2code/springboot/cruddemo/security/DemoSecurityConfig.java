ee3e5dafa10bcad5d12e741bdf71978d527362a2
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2023-09-25 15:04:43 -0500

    v153

--- Springboot/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ Springboot/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -8,18 +8,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder().username("john").password("{noop}test123").roles("Employee").build();
-        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("Employee", "Manager").build();
-        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("Employee", "Manager", "Admin").build();
-
-        return new InMemoryUserDetailsManager(john,mary,susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return  new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -36,4 +35,12 @@ public class DemoSecurityConfig {
         http.csrf(csrf -> csrf.disable());
         return http.build();
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder().username("john").password("{noop}test123").roles("Employee").build();
+//        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("Employee", "Manager").build();
+//        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("Employee", "Manager", "Admin").build();
+//        return new InMemoryUserDetailsManager(john,mary,susan);
+//    }
 }

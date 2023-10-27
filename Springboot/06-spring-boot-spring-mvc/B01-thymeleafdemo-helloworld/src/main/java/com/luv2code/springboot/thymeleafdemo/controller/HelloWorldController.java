eb7f8697ea010d127738c658b6167c49290c5dd1
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2023-10-27 14:27:55 -0500

    v181

--- Springboot/06-spring-boot-spring-mvc/B01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ Springboot/06-spring-boot-spring-mvc/B01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloWorld-form";
     }

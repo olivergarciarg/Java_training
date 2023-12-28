a3fa12032c3a38112e20af412620e40a4e929b0e
Oliver Garcia <46539206+olivergarciarg@users.noreply.github.com>
2023-12-28 14:14:20 -0600

    s10v366

--- Springboot/10-spring-boot-aop/00-spring-boot-aop-before-advice/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Springboot/10-spring-boot-aop/00-spring-boot-aop-before-advice/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -4,10 +4,12 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
     // this is where we add all of our related advices for logging
     // let's start with @Before advice
@@ -16,20 +18,20 @@ public class MyDemoLoggingAspect {
 //        System.out.println("\n=====>>> Executing @Before advice on addAccount()");
 //    }
 
-    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-    private void forDaoPackage() {}
-
-    // create pointcut for getter methods
-    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
-    private void getter() {}
-
-    // create pointcut for setter methods
-    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
-    private void setter() {}
-
-    // create pointcut: include package ... exclude getter/setter
-    @Pointcut("forDaoPackage() && !(getter() || setter())")
-    private void forDaoPackageNoGetterSetter() {}
+//    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+//    private void forDaoPackage() {}
+//
+//    // create pointcut for getter methods
+//    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+//    private void getter() {}
+//
+//    // create pointcut for setter methods
+//    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+//    private void setter() {}
+//
+//    // create pointcut: include package ... exclude getter/setter
+//    @Pointcut("forDaoPackage() && !(getter() || setter())")
+//    private void forDaoPackageNoGetterSetter() {}
 
 //    @Before("execution(public void add*())")
 //    @Before("execution(* add*())")
@@ -38,14 +40,19 @@ public class MyDemoLoggingAspect {
 //    @Before("execution(* add*(..))")
 //    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 //    @Before("forDaoPackage()")
-    @Before("forDaoPackageNoGetterSetter()")
+    @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
     }
 
 //    @Before("forDaoPackage()")
-    @Before("forDaoPackageNoGetterSetter()")
-    public void performApiAnalytics() {
-        System.out.println("\n=====>>> Performing API analytics");
-    }
+//    @Before("forDaoPackageNoGetterSetter()")
+//    public void performApiAnalytics() {
+//        System.out.println("\n=====>>> Performing API analytics");
+//    }
+
+//    @Before("forDaoPackageNoGetterSetter()")
+//    public void logToCloudAsync() {
+//        System.out.println("\n=====>>> Logging to Cloud in async fashion");
+//    }
 }
